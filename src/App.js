274b335e253c2bf62279418160f1192c661ca2e9
Lina Beltrán <lmbd92@hotmail.com>
2023-11-22 14:32:42 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Navbar from "./components/navbar/navbar";
+import Home from "./components/home/home";
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Home />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/navbar/navbar.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./navbar.css";
+
+const Navbar = () => {
+  return (
+    <nav>
+      <h1>Lina Beltran Dev</h1>
+      <ul>
+        <li>Home</li>
+        <li>Projects</li>
+        <li>About me</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
